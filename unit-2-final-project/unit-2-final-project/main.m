00ba2ee8a05ad6ce510b5b0d446e803f10cb0d2b
charles <ckangkitchen@gmail.com>
2015-10-10 10:52:35 -0400

    first commit

---
+++ unit-2-final-project/unit-2-final-project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  unit-2-final-project
+//
+//  Created by Charles Kang on 10/10/15.
+//  Copyright © 2015 Charles Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

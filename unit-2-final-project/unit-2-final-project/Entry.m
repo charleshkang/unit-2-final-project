da0b46d2fc7e0626e3e715d3a65dd8c3e675fab1
Lauren <laurencaponong@gmail.com>
2015-10-11 16:30:53 -0400

    Started putting together journal functionality

---
+++ unit-2-final-project/unit-2-final-project/Entry.m
@@ -0,0 +1,15 @@
+//
+//  Entry.m
+//  unit-2-final-project
+//
+//  Created by Lauren Caponong on 10/11/15.
+//  Copyright © 2015 Charles Kang. All rights reserved.
+//
+
+#import "Entry.h"
+
+@implementation Entry
+
+// Insert code here to add functionality to your managed object subclass
+
+@end

2a409312282d5631613b7cdefbd8795799f0e43d
charles <ckangkitchen@gmail.com>
2015-10-13 20:13:16 -0400

    deleted entrycreationtableviewcontroller

--- cloud/cloud/main.js
+++ cloud/cloud/main.js
@@ -4,14 +4,14 @@ var twilio = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 Parse.Cloud.define("send_twilio_message", function(request, response) {
 
-  var User = Parse.Object.extend("LinkedInUser");
+  var User = Parse.Object.extend("User");
   var query = new Parse.Query(User);
   query.find({
     success: function(results) {
       twilio.sms.messages.create({
-          to: "+19177534949",
+          to: "+19494916672",
           from: "+12016907828",
-          body: "what's up lauren" + results[0].get('userId')
+          body: "testing!" + results[0].get('userId')
         }, function(error, msg) {
           if (error) {
             response.error(error);
@@ -42,3 +42,9 @@ curl -X POST \
  -d '{}' \
  https://api.parse.com/1/functions/send_twilio_message
  */
+
+
+/*
+$ curl https://taskrouter.twilio.com/v1/Workspaces/{WorkspaceSid}/TaskQueues/{QU11552c4a270543a195ed2461e1320cd9} \
+    -u '{AC66c9ea5429517d983d0f9eec0b7adcc4}:{c77ff9cd7f6efaa52272f6f807f4c171}'
+    */
